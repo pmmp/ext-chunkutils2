7f24ef4a6398c50eead1344420a9845920123174
Dylan K. Taylor <odigiman@gmail.com>
2020-09-13 14:19:38 +0100

    ZendUtil: use emalloc for object allocation instead of ecalloc, closes #17 this brings a 25% performance improvement to the creation of new LightArray objects. Overall this isn't really noticeable, but it's nice to have anyway.

--- src/ZendUtil.h
+++ src/ZendUtil.h
@@ -12,7 +12,7 @@ static inline class_name * fetch_from_zend_object(zend_object *obj) {
 
 template<typename class_name>
 static class_name* alloc_custom_zend_object(zend_class_entry* ce, zend_object_handlers *handlers) {
-	class_name* object = (class_name*)ecalloc(1, sizeof(class_name) + zend_object_properties_size(ce));
+	class_name* object = (class_name*)emalloc(sizeof(class_name) + zend_object_properties_size(ce));
 
 	zend_object_std_init(&object->std, ce);
 	object_properties_init(&object->std, ce);
