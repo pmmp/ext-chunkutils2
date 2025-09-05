77402387e9082f103585b3090162cad8dbd247b7
Dylan K. Taylor <dktapps@pmmp.io>
2025-09-05 22:23:34 +0100

    Fix stubs for <8.4

--- stubs/pocketmine/world/format/PalettedBlockArrayLoadException.stub.php
+++ stubs/pocketmine/world/format/PalettedBlockArrayLoadException.stub.php
@@ -2,6 +2,7 @@
 
 /**
  * @generate-class-entries
+ * @generate-legacy-arginfo 80100
  */
 
 namespace pocketmine\world\format;
--- stubs/pocketmine/world/format/PalettedBlockArrayLoadException_arginfo.h
+++ stubs/pocketmine/world/format/PalettedBlockArrayLoadException_arginfo.h
@@ -1,12 +1,16 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 99204f020e50c95977c6991a1fd9e5e72348ec77 */
+ * Stub hash: a2c6d79d299cd37711158413ec1e33242e405370 */
 
 static zend_class_entry *register_class_pocketmine_world_format_PalettedBlockArrayLoadException(zend_class_entry *class_entry_RuntimeException)
 {
 	zend_class_entry ce, *class_entry;
 
 	INIT_NS_CLASS_ENTRY(ce, "pocketmine\\world\\format", "PalettedBlockArrayLoadException", NULL);
+#if (PHP_VERSION_ID >= 80400)
 	class_entry = zend_register_internal_class_with_flags(&ce, class_entry_RuntimeException, 0);
+#else
+	class_entry = zend_register_internal_class_ex(&ce, class_entry_RuntimeException);
+#endif
 
 	return class_entry;
 }
